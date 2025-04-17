b7a7d630744c5cc47013603ea3fae0afa8dc9fab
TheBenda <andre.benda@jambit.com>
2025-04-17 10:59:12 +0200

    Add base architecture and upgrade essential packages

--- aspire/ALB.AppHost/Program.cs
+++ aspire/ALB.AppHost/Program.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithPgWeb()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+builder.AddProject<Projects.ALB_Api>("Api")
+    .WithReference(postgresdb)
+    .WaitFor(postgresdb);
+
 builder.Build().Run();
