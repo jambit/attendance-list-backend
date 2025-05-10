f103583dff026f2da6555b1da5e33a8bf99afc20
NicasFrank <nicas.frank@gmail.com>
2025-05-10 21:19:02 +0200

    17 Implemented tests for UserEndpoints

--- tests/ApiIntegrationTests/ApiIntegrationTests/BaseIntegrationTest.cs
+++ tests/ApiIntegrationTests/ApiIntegrationTests/BaseIntegrationTest.cs
@@ -1,14 +1,18 @@
+using System.Security.Claims;
+using System.Text.Encodings.Web;
 using ALB.Api;
-using ALB.Api.Extensions;
 using ALB.Domain.Identity;
 using ALB.Domain.Values;
 using ALB.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Testcontainers.PostgreSql;
 using TUnit.Core.Interfaces;
 
@@ -25,10 +29,10 @@ public class BaseIntegrationTest : IAsyncInitializer, IAsyncDisposable
         .WithDatabase("postgres")
         .Build();
 
-    private WebApplicationFactory<IApiAssemblyMarker> _webApplicationFactory = default!;
+    private WebApplicationFactory<IApiAssemblyMarker> _webApplicationFactory = null!;
 
-    private RoleManager<ApplicationRole> _roleManager = default!;
-    private UserManager<ApplicationUser> _userManager = default!;
+    private RoleManager<ApplicationRole> _roleManager = null!;
+    private UserManager<ApplicationUser> _userManager = null!;
     
     private HttpClient GetHttpClient(string token)
     {
@@ -43,8 +47,16 @@ public class BaseIntegrationTest : IAsyncInitializer, IAsyncDisposable
     }
 
     public HttpClient GetAdminClient()
-        => GetHttpClient("Admin");
+        => GetHttpClient(SystemRoles.Admin);
     
+    public HttpClient GetCoAdminClient()
+        => GetHttpClient(SystemRoles.CoAdmin);
+    
+    public HttpClient GetTeamClient()
+        => GetHttpClient(SystemRoles.Team);
+    
+    public HttpClient GetParentClient()
+        => GetHttpClient(SystemRoles.Parent);
     
     public async Task InitializeAsync()
     {
@@ -65,6 +77,7 @@ public class BaseIntegrationTest : IAsyncInitializer, IAsyncDisposable
 
         await SeedDatabase();
     }
+    
     
     private async Task SeedDatabase()
     {
@@ -110,6 +123,46 @@ public class BaseIntegrationTest : IAsyncInitializer, IAsyncDisposable
     }
 }
 
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger, UrlEncoder encoder)
+        : base(options, logger, encoder)
+    {}
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var apiKey = Request.Headers["X-Api-Key"].ToString();
+        
+        Console.WriteLine(apiKey);
+
+        var claims = apiKey switch
+        {
+            SystemRoles.Admin => new[]
+            {
+                new Claim(ClaimTypes.Name, "AdminUser"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            SystemRoles.Parent => new[]
+            {
+                new Claim(ClaimTypes.Name, "ParentUser"),
+                new Claim(ClaimTypes.Role, "Parent")
+            },
+            _ => throw new InvalidOperationException("Unknown API Key")
+        };
+        
+        Console.WriteLine(apiKey);
+        
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
+
 file sealed class TestWebApplicationFactory(string connectionString) : WebApplicationFactory<IApiAssemblyMarker>
 {
     protected override IHost CreateHost(IHostBuilder builder)
@@ -141,8 +194,18 @@ file sealed class TestWebApplicationFactory(string connectionString) : WebApplic
             {
                 opts.UseNpgsql(connectionString, x => x.UseNodaTime());
             });
+            
+            services.AddAuthentication("TestScheme")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestScheme", options => { });
+
+            services.AddAuthorizationBuilder()
+                .AddPolicy(SystemRoles.AdminPolicy, x => x.RequireRole(SystemRoles.Admin))
+                .AddPolicy(SystemRoles.CoAdminPolicy, x => x.RequireRole(SystemRoles.CoAdmin))
+                .AddPolicy(SystemRoles.TeamPolicy, x => x.RequireRole(SystemRoles.Team))
+                .AddPolicy(SystemRoles.ParentPolicy, x => x.RequireRole(SystemRoles.Parent));
         });
 
         return base.CreateHost(builder);
     }
+
 }
--- tests/ApiIntegrationTests/ApiIntegrationTests/Users/UsersEndpointsTests.cs
+++ tests/ApiIntegrationTests/ApiIntegrationTests/Users/UsersEndpointsTests.cs
@@ -1,32 +1,36 @@
 using System.Net;
 using System.Net.Http.Json;
-using ALB.Api.UseCases.ExampleFeatures.Endpoints.Create;
-using Microsoft.AspNetCore.Identity.Data;
+using ALB.Api.UseCases.Users.Endpoints.Create;
 
 namespace ApiIntegrationTests.Users;
 
 [ClassDataSource<BaseIntegrationTest>(Shared = SharedType.PerAssembly)]
 public class UsersEndpointsTests(BaseIntegrationTest baseIntegrationTest)
 {
+    
+    private HttpClient ParentClient => baseIntegrationTest.GetParentClient();
+    private HttpClient AdminClient => baseIntegrationTest.GetAdminClient();
+
+    private readonly CreateUserRequest _createUserRequest = new CreateUserRequest{
+        Email = "user@test.com",
+        Password = "SuperSecurePassword123!",
+        FirstName = "Max",
+        LastName = "Mustermann",
+    };
+
     [Test]
-    public async Task Should_Return_NotFound_When_User_Is_Not_Found_On_Role_Assignment()
+    public async Task Should_Create_User_Successfully()
     {
-        // Login with admin
-        var client = baseIntegrationTest.GetAdminClient();
-        var response = await client.PostAsJsonAsync("/login", new LoginRequest
-        {
-            Email = BaseIntegrationTest.AdminEmail,
-            Password = BaseIntegrationTest.AdminPassword
-        });
-        
-        //var response = await client.PostAsJsonAsync("/examples", new CreateExampleRequest("test"));
+        var response = await AdminClient.PostAsJsonAsync("api/users", _createUserRequest);
         
         await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
-        // Create new User
-        // => post /users
-        // Set his Role
-        // => post /users/roles
+    }
+
+    [Test]
+    public async Task Should_Return_Unauthorized_When_Non_Admin_Is_Creating()
+    {
+        var response = await ParentClient.PostAsJsonAsync("api/users", _createUserRequest);
         
-        // TODO: finish Test => take token and stuff to reach endpoint.
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.Forbidden);
     }
 }
