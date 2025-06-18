6921a6010806663b950a6dc4162b951f849e1d71
Steven <pa19duxu@studserv.uni-leipzig.de>
2025-06-18 11:47:40 +0200

    added JsonIgnore (#54)
    fix for OpenApiScheme

--- src/ALB.Domain/Entities/Group.cs
+++ src/ALB.Domain/Entities/Group.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using ALB.Domain.Identity;
 
 namespace ALB.Domain.Entities;
@@ -9,7 +10,10 @@ public class Group
     public Guid ResponsibleUserId { get; set; }
     
     public ApplicationUser ResponsibleUser { get; set; } = null!;
+    [JsonIgnore]
     public ICollection<Child> Children { get; set; } = new List<Child>();
+    [JsonIgnore]
     public ICollection<Cohort> Cohorts { get; set; } = new List<Cohort>();
+    [JsonIgnore]
     public ICollection<UserGroup> UserGroups { get; set; } = new List<UserGroup>();
 }
