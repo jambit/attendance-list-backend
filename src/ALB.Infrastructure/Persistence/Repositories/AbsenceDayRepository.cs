791450b60d87caa46f8a2e7f0a709f309c31e66a
jannikspaeth <jannik.spaeth@gmx.de>
2025-06-24 15:09:57 +0200

    SetAbsence range
    Parents can set a range of absence for their child

--- src/ALB.Infrastructure/Persistence/Repositories/AbsenceDayRepository.cs
+++ src/ALB.Infrastructure/Persistence/Repositories/AbsenceDayRepository.cs
@@ -26,4 +26,16 @@ public class AbsenceDayRepository : IAbsenceDayRepository
         return await _dbContext.AbsenceDays
             .AnyAsync(a => a.ChildId == childId && a.Date == date);
     }
+    
+    public async Task AddRangeAsync(IEnumerable<AbsenceDay> absenceDays, CancellationToken cancellationToken)
+    {
+        await _dbContext.AbsenceDays.AddRangeAsync(absenceDays, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+    
+    public async Task<bool> ExistsInRangeAsync(Guid childId, LocalDate startDate, LocalDate endDate, CancellationToken cancellationToken)
+    {
+        return await _dbContext.AbsenceDays
+            .AnyAsync(ad => ad.ChildId == childId && ad.Date >= startDate && ad.Date <= endDate, cancellationToken);
+    }
 }
